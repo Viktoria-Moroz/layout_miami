b3e7f5b1b0272d607c4c65c9a2b68236599445ac
Viktoria Moroz <viktoriaandriivna25@gmail.com>
2023-02-24 12:33:55 +0200

    make build

--- src/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#menu') {
-    document.body.classList.add('content--with-menu');
-  } else {
-    document.body.classList.remove('content--with-menu');
-  }
-});
